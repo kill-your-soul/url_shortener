a52494414e9dd6c21f702204268debcf8ef1cf70
kill-your-soul <Platonovdi@gmail.com>
2023-11-27 23:12:36 +0300

    Now serializer return absolute uri in short_url

--- url_shortener/serializers.py
+++ url_shortener/serializers.py
@@ -6,3 +6,10 @@ class ShortenedURLSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShrotenedURL
         fields = ["original_url", "short_url"]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["short_url"] = self.context["request"].build_absolute_uri(
+            f"/api/{data['short_url']}"
+        )
+        return data
